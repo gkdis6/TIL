225d6ff21ab52d78cf78427cc699cecafe2c5bf7
SangYong <gkdis6@gmail.com>
2021-09-16 13:02:10 +0900

    add javascript

--- javascript/variable.js
+++ javascript/variable.js
@@ -4,3 +4,23 @@ const maxNumber = 5;
 
 console.log(name);
 console.log(maxNumber);
+
+let a = 12;
+let b = 12.1;
+
+console.log(`value:${a}, type:${typeof a}`);
+console.log(`value:${b}, type:${typeof b}`);
+
+const infinity = 1/0;
+const negativeInfinity = -1/0;
+const nAn = 'not a number'/2;
+
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+const char = 'c';
+const brendan = "brendan";
+const greeting = "hello " + "brendan";
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+
