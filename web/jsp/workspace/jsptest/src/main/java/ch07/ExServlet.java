1f9b417b85f4b2b785ffd9c1f5628521f40dce06
SangYong <gkdis6@gmail.com>
2021-09-29 22:44:38 +0900

    edit servlet

--- web/jsp/workspace/jsptest/src/main/java/ch07/ExServlet.java
+++ web/jsp/workspace/jsptest/src/main/java/ch07/ExServlet.java
@@ -1,6 +1,8 @@
 package ch07;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,7 +20,13 @@ public class ExServlet extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		//response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h1>이클립스로 서블릿 만들기</h1>");
+		out.println("</body>");
+		out.println("</html>");
 	}
 
 }
