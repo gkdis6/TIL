f3956f6024885b1ec91d83e96a06954a0af3e770
SangYong <gkdis6@gmail.com>
2022-01-18 19:57:03 +0900

    add 면접

--- java/test_encrypt/src/encrypt/Main.java
+++ java/test_encrypt/src/encrypt/Main.java
@@ -3,23 +3,42 @@ package encrypt;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main {
-
-	public static StringBuilder sb = new StringBuilder();
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
+public class Main{
+	static int[][] pays, cost;
+	static int finish;
+	static int red = 0;
+	static int green = 1;
+	static int blue = 2;
+    public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 
-		int a = Integer.parseInt(br.readLine());
-		int b = 1;
-		int c = 1;
-		while(a/b>=1) {
-			b = b*10;
-			c++;
+		int n = Integer.parseInt(br.readLine());
+		finish = n;
+		pays = new int[n][3];
+		cost = new int[n][3];
+		for(int i = 0; i<n; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			pays[i][0] = Integer.parseInt(st.nextToken());
+			pays[i][1] = Integer.parseInt(st.nextToken());
+			pays[i][2] = Integer.parseInt(st.nextToken());
 		}
-        System.out.println(b);
-        System.out.println(c);
-        
-	}
+		cost[0][red] = pays[0][red];
+		cost[0][green] = pays[0][green];
+		cost[0][blue] = pays[0][blue];
+		
+		dfs(1);
+		
+		System.out.println(Math.min(cost[n-1][0], Math.min(cost[n-1][1], cost[n-1][2])));
+    }
+    
+    static void dfs(int depth) {
+    	if(depth == finish) {
+    		return;
+    	}
+    	for(int i = 0; i<3; i++) {
+    		cost[depth][i] = pays[depth][i] + Math.min(cost[depth-1][(i+1)%3], cost[depth-1][(i+2)%3]); 
+    	}
+    	dfs(depth+1);
+    }
 }
