dc58432e9a0f61a6037e1db0adc16c2766d6eed9
SangYong <gkdis6@gmail.com>
2022-05-30 00:37:08 +0900

    edit monolithic_goods

--- nodejs_micro_service/monolithic_goods.js
+++ nodejs_micro_service/monolithic_goods.js
@@ -21,3 +21,29 @@ exports.onRequest = function(res, method, pathname, params, cb){
             return process.nextTick(cb, res, response);
     }
 }
+
+function register(method, pathname, params, cb){
+    var response = {
+        errorcode: 0,
+        errormessage: "success"
+    };
+
+    if(params.name == null || params.category == null || params.price == null || params.description == null){
+        response.errorcode = 1;
+        response.errormessage = "Invalid Parameters";
+        cb(response);
+    }else{
+        var connection = mysql.createConnection(conn);
+        connection.connect();
+        connection.query("insert into goods(name, category, price, description) values(?,?,?,?)"
+        , [params.name, params.category, params.price, params.description]
+        , (error, results, fields) => {
+            if(error){
+                response.errorcode = 1;
+                response.errormessage = error;
+            }
+            cb(response);
+        });
+        connenction.end();
+    }
+}
