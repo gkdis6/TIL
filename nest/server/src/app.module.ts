b9c3fe01b8aa7c87067dfe07991c17e6fc4d9d8f
SangYong <gkdis6@gmail.com>
2024-07-21 00:49:54 +0900

    nest dotenv 추가

--- nest/server/src/app.module.ts
+++ nest/server/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ServerModule } from './server/server.module';
+import { ConfigModule } from '@nestjs/config';
+import Configuration from './config/env/configuration';
 
 @Module({
-  imports: [ServerModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [Configuration],
+      isGlobal: true,
+    }),
+    ServerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
