a0fdc806c148fdfb2075b7d5da8c2102831e08e6
SangYong <gkdis6@gmail.com>
2024-07-21 18:16:57 +0900

    nest pipeline 추가

--- nest/server/src/main.ts
+++ nest/server/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import configuration from './config/env/configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(configuration().port);
 }
 bootstrap();
