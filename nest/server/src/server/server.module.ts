2c840df75f9b61c3f6a9a1039b140fcf6c6ae6f6
SangYong <gkdis6@gmail.com>
2024-07-22 00:16:03 +0900

    nest module entity 연결

--- nest/server/src/server/server.module.ts
+++ nest/server/src/server/server.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ServerService } from './server.service';
 import { ServerController } from './server.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Server } from './entities/server.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Server])],
   controllers: [ServerController],
   providers: [ServerService],
 })
