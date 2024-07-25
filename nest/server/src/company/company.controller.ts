50d250c2df6044bb78c15999d28516dafa06df92
SangYong <gkdis6@gmail.com>
2024-07-25 21:59:51 +0900

    nest company controller swagger api tag 추가

--- nest/server/src/company/company.controller.ts
+++ nest/server/src/company/company.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Company } from './entities/company.entity';
 
 @Controller('company')
+@ApiTags('회사 API')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
   @Post()
+  @ApiOperation({ summary: '회사 생성 API', description: '회사를 생성한다.' })
+  @ApiCreatedResponse({
+    description: '회사를 생성한다.',
+    type: Company,
+  })
   create(@Body() createCompanyDto: CreateCompanyDto) {
     return this.companyService.create(createCompanyDto);
   }
